1e2a9fac6df60c4088c26a9d376058741669fb31
rchua-matc <rchua@madisoncollege.edu>
2022-01-09 13:09:48 -0600

    docs: contact card

--- lib/widgets/contact_card.dart
+++ lib/widgets/contact_card.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+/// A material design card to display contact information.
+///
+/// Based on the [Card] widget. The [icon] is placed at the leading edge and
+/// [text] is used as the title. When tapped, the corresponding [url] will
+/// attempt to open.
 class ContactCard extends StatelessWidget {
-  ContactCard({required this.icon, required this.text, required this.url});
+  /// Creates a contact card.
+  ///
+  /// The [url] should include the appropriate prefix for the contact link,
+  /// e.g., a telephone link begins with 'tel:'.
+  const ContactCard({
+    Key? key,
+    required this.icon,
+    required this.text,
+    required this.url,
+  });
 
+  /// The card's icon.
   final IconData icon;
+
+  /// The card's displayed text.
   final String text;
+
+  /// The card's linked contact.
   final String url;
 
+  /// Attempts to open the card's linked [url].
+  ///
+  /// Throws an exception if the contact link is not able to be launched.
   void launchContact() async {
     if (!await launch(url)) throw 'Could not launch $url';
   }
